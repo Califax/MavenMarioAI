d09d9eba69a4587054cb7d32bd2d26af26e2fa66
Kyle <kyldvs@gmail.com>
2013-02-28 18:47:19 -0500

    Remove their shitty code and add functionality to MyLevel

--- src/dk/itu/mario/level/MyLevel.java
+++ src/dk/itu/mario/level/MyLevel.java
@@ -1,5 +1,6 @@
 package dk.itu.mario.level;
 
+import java.awt.Point;
 import java.util.Random;
 
 import dk.itu.mario.MarioInterface.GamePlay;
@@ -9,386 +10,182 @@ import dk.itu.mario.engine.sprites.SpriteTemplate;
 
 public class MyLevel extends Level {
 	// Store information about the level
-	public int ENEMIES = 0; // the number of enemies the level contains
-	public int BLOCKS_EMPTY = 0; // the number of empty blocks
-	public int BLOCKS_COINS = 0; // the number of coin blocks
-	public int BLOCKS_POWER = 0; // the number of power blocks
-	public int COINS = 0; // These are the coins in boxes that Mario collect
-
-	private static Random levelSeedRandom = new Random();
-	public static long lastSeed;
-
-	Random random;
-
+	private Random random;
 	private int difficulty;
 	private int type;
-	private int gaps;
 
 	public MyLevel(int width, int height) {
 		super(width, height);
 	}
 
-	public MyLevel(int width, int height, long seed, int difficulty, int type,
-			GamePlay playerMetrics) {
+	public MyLevel(int width, int height, long seed, int difficulty, int type, GamePlay playerMetrics) {
 		this(width, height);
-		create(seed, difficulty, type);
-
-		System.out.println(playerMetrics);
+		init(seed, difficulty, type);
+		create();
 	}
 
-	public void create(long seed, int difficulty, int type) {
+	public void init(long seed, int difficulty, int type) {
 		this.type = type;
 		this.difficulty = difficulty;
+		this.random = new Random(seed);
+	}
 
-		lastSeed = seed;
-		random = new Random(seed);
-
+	public void create() {
 		// create the start location
-		int length = 0;
-		length += buildStraight(0, width, true);
+		Point at = new Point(0, 2);
+		while(at.x < 100) {
+			if (at.x % 10 == 8) {
+				at = jump(at, 1);
+				at.y = random.nextInt(4) + 1;
+			} else {
+				at = straight(at, 1);
+			}
+			
+			if (at.x > 20 && at.x % 21 == 4) {
+				pipe(at, 1, random.nextBoolean());
+			}
+			
+			if (at.x % 31 == 0) {
+				enemy(at, Enemy.ENEMY_GOOMBA, random.nextBoolean());
+			}
+		}
 
 		// create all of the medium sections
-		while (length < width - 64) {
-			// length += buildZone(length, width - length);
-			length += buildStraight(length, width - length, false);
-			length += buildStraight(length, width - length, false);
-			length += buildHillStraight(length, width - length);
-			length += buildJump(length, width - length);
-			length += buildTubes(length, width - length);
-			length += buildCannons(length, width - length);
-		}
+		//		while (length < width - 64) {
+		//			length += buildStraight(length, width - length, false);
+		//        	length += buildZone(length, width - length);
+		//			length += buildStraight(length, width - length, false);
+		//			length += buildHillStraight(length, width - length);
+		//			length += buildJump(length, width - length);
+		//			length += buildTubes(length, width - length);
+		//			length += buildCannons(length, width - length);
+		//		}
 
 		// set the end piece
-		int floor = height - 1 - random.nextInt(4);
-
-		xExit = length + 8;
-		yExit = floor;
-
-		// fills the end piece
-		for (int x = length; x < width; x++) {
-			for (int y = 0; y < height; y++) {
-				if (y >= floor) {
-					setBlock(x, y, GROUND);
-				}
-			}
-		}
-
-		if (type == LevelInterface.TYPE_CASTLE
-				|| type == LevelInterface.TYPE_UNDERGROUND) {
-			int ceiling = 0;
-			int run = 0;
-			for (int x = 0; x < width; x++) {
-				if (run-- <= 0 && x > 4) {
-					ceiling = random.nextInt(4);
-					run = random.nextInt(4) + 4;
-				}
-				for (int y = 0; y < height; y++) {
-					if ((x > 4 && y <= ceiling) || x < 1) {
-						setBlock(x, y, GROUND);
-					}
-				}
-			}
-		}
-
+//		int floor = height - 1 - random.nextInt(4);
+//		xExit = x + 8;
+//		yExit = floor;
+//
+//		// fills the end piece
+//		for (int i = x; i < width; i++) {
+//			for (int y = 0; y < height; y++) {
+//				if (y >= floor) {
+//					setBlock(i, y, GROUND);
+//				}
+//			}
+//		}
+//
+//		if (type == LevelInterface.TYPE_CASTLE
+//				|| type == LevelInterface.TYPE_UNDERGROUND) {
+//			int ceiling = 0;
+//			int run = 0;
+//			for (int i = 0; i < width; i++) {
+//				if (run-- <= 0 && i > 4) {
+//					ceiling = random.nextInt(4);
+//					run = random.nextInt(4) + 4;
+//				}
+//				for (int y = 0; y < height; y++) {
+//					if ((i > 4 && y <= ceiling) || i < 1) {
+//						setBlock(i, y, GROUND);
+//					}
+//				}
+//			}
+//		}
+		
+		/*
+		 * coordinates of the exit
+		 */
+		xExit = at.x;
+		yExit = y(at.y - 1);
+		straight(at, width - at.x);
 		fixWalls();
-
 	}
 
-	private int buildJump(int xo, int maxLength) {
-		gaps++;
-		// jl: jump length
-		// js: the number of blocks that are available at either side for free
-		int js = random.nextInt(4) + 2;
-		int jl = random.nextInt(2) + 2;
-		int length = js * 2 + jl;
-
-		boolean hasStairs = random.nextInt(3) == 0;
-
-		int floor = height - 1 - random.nextInt(4);
-		// run from the start x position, for the whole length
-		for (int x = xo; x < xo + length; x++) {
-			if (x < xo + js || x > xo + length - js - 1) {
-				// run for all y's since we need to paint blocks upward
-				for (int y = 0; y < height; y++) { // paint ground up until the
-													// floor
-					if (y >= floor) {
-						setBlock(x, y, GROUND);
-					}
-					// if it is above ground, start making stairs of rocks
-					else if (hasStairs) { // LEFT SIDE
-						if (x < xo + js) { // we need to max it out and level
-											// because it wont
-											// paint ground correctly unless two
-											// bricks are side by side
-							if (y >= floor - (x - xo) + 1) {
-								setBlock(x, y, ROCK);
-							}
-						} else { // RIGHT SIDE
-							if (y >= floor - ((xo + length) - x) + 2) {
-								setBlock(x, y, ROCK);
-							}
-						}
-					}
-				}
+	/**
+	 * Build straight section at given point with specified length
+	 */
+	private Point straight(Point at, int l) {
+		for (int i = 0; i < l; i++) {
+			for (int j = 0; j < at.y; j++) {
+				setBlock(at.x + i, y(j), GROUND);
 			}
 		}
-
-		return length;
+		at.x += l;
+		return at;
+	}
+	
+	/**
+	 * Creates a jump of the specified length at the given point
+	 */
+	private Point jump(Point at, int l) {
+		at.x += l;
+		at.y = 0;
+		return at;
 	}
 
-	private int buildCannons(int xo, int maxLength) {
-		int length = random.nextInt(10) + 2;
-		if (length > maxLength)
-			length = maxLength;
-
-		int floor = height - 1 - random.nextInt(4);
-		int xCannon = xo + 1 + random.nextInt(4);
-		for (int x = xo; x < xo + length; x++) {
-			if (x > xCannon) {
-				xCannon += 2 + random.nextInt(4);
-			}
-			if (xCannon == xo + length - 1)
-				xCannon += 10;
-			int cannonHeight = floor - random.nextInt(4) - 1;
-
-			for (int y = 0; y < height; y++) {
-				if (y >= floor) {
-					setBlock(x, y, GROUND);
-				} else {
-					if (x == xCannon && y >= cannonHeight) {
-						if (y == cannonHeight) {
-							setBlock(x, y, (byte) (14 + 0 * 16));
-						} else if (y == cannonHeight + 1) {
-							setBlock(x, y, (byte) (14 + 1 * 16));
-						} else {
-							setBlock(x, y, (byte) (14 + 2 * 16));
-						}
-					}
-				}
-			}
-		}
-
-		return length;
-	}
-
-	private int buildHillStraight(int xo, int maxLength) {
-		int length = random.nextInt(10) + 10;
-		if (length > maxLength)
-			length = maxLength;
-
-		int floor = height - 1 - random.nextInt(4);
-		for (int x = xo; x < xo + length; x++) {
-			for (int y = 0; y < height; y++) {
-				if (y >= floor) {
-					setBlock(x, y, GROUND);
-				}
-			}
-		}
-
-		addEnemyLine(xo + 1, xo + length - 1, floor - 1);
-
-		int h = floor;
-
-		boolean keepGoing = true;
-
-		boolean[] occupied = new boolean[length];
-		while (keepGoing) {
-			h = h - 2 - random.nextInt(3);
-
-			if (h <= 0) {
-				keepGoing = false;
+	/**
+	 * Buids a pipe at the given point of the height, if flower is true there will be a flower enemy in the pipe
+	 * 
+	 * @param at
+	 * @param h
+	 * @param flower
+	 */
+	private void pipe(Point at, int h, boolean flower) {
+		for (int i = 0; i < h; i++) {
+			if (i == h - 1) {
+				setBlock(at.x, y(at.y + i), TUBE_TOP_LEFT);
+				setBlock(at.x + 1, y(at.y + i), TUBE_TOP_RIGHT);
 			} else {
-				int l = random.nextInt(5) + 3;
-				int xxo = random.nextInt(length - l - 2) + xo + 1;
-
-				if (occupied[xxo - xo] || occupied[xxo - xo + l]
-						|| occupied[xxo - xo - 1] || occupied[xxo - xo + l + 1]) {
-					keepGoing = false;
-				} else {
-					occupied[xxo - xo] = true;
-					occupied[xxo - xo + l] = true;
-					addEnemyLine(xxo, xxo + l, h - 1);
-					if (random.nextInt(4) == 0) {
-						decorate(xxo - 1, xxo + l + 1, h);
-						keepGoing = false;
-					}
-					for (int x = xxo; x < xxo + l; x++) {
-						for (int y = h; y < floor; y++) {
-							int xx = 5;
-							if (x == xxo)
-								xx = 4;
-							if (x == xxo + l - 1)
-								xx = 6;
-							int yy = 9;
-							if (y == h)
-								yy = 8;
-
-							if (getBlock(x, y) == 0) {
-								setBlock(x, y, (byte) (xx + yy * 16));
-							} else {
-								if (getBlock(x, y) == HILL_TOP_LEFT)
-									setBlock(x, y, HILL_TOP_LEFT_IN);
-								if (getBlock(x, y) == HILL_TOP_RIGHT)
-									setBlock(x, y, HILL_TOP_RIGHT_IN);
-							}
-						}
-					}
-				}
+				setBlock(at.x, y(at.y + i), TUBE_SIDE_LEFT);
+				setBlock(at.x + 1, y(at.y + i), TUBE_SIDE_RIGHT);
 			}
 		}
-
-		return length;
+		
+		if (flower) {
+			setSpriteTemplate(at.x, y(at.y + h - 1), new SpriteTemplate(Enemy.ENEMY_FLOWER, false));
+		}
 	}
-
-	private void addEnemyLine(int x0, int x1, int y) {
-		for (int x = x0; x < x1; x++) {
-			if (random.nextInt(35) < difficulty + 1) {
-				int type = random.nextInt(4);
-
-				if (difficulty < 1) {
-					type = Enemy.ENEMY_GOOMBA;
-				} else if (difficulty < 3) {
-					type = random.nextInt(3);
-				}
-
-				setSpriteTemplate(x, y,
-						new SpriteTemplate(type,
-								random.nextInt(35) < difficulty));
-				ENEMIES++;
+	
+	/**
+	 * Builds a cannon of the specified height at the point
+	 */
+	private void cannon(Point at, int h) {
+		for (int i = 0; i < h; i++) {
+			if (i == h - 1) {
+				setBlock(at.x, y(at.y + i), CANNON_TOP);
+			} else if (i == h - 2) {
+				setBlock(at.x, y(at.y + i), CANNON_MID);
+			} else {
+				setBlock(at.x, y(at.y + i), CANNON_BOT);
 			}
 		}
 	}
 
-	private int buildTubes(int xo, int maxLength) {
-		int length = random.nextInt(10) + 5;
-		if (length > maxLength)
-			length = maxLength;
-
-		int floor = height - 1 - random.nextInt(4);
-		int xTube = xo + 1 + random.nextInt(4);
-		int tubeHeight = floor - random.nextInt(2) - 2;
-		for (int x = xo; x < xo + length; x++) {
-			if (x > xTube + 1) {
-				xTube += 3 + random.nextInt(4);
-				tubeHeight = floor - random.nextInt(2) - 2;
-			}
-			if (xTube >= xo + length - 2)
-				xTube += 10;
-
-			if (x == xTube && random.nextInt(11) < difficulty + 1) {
-				setSpriteTemplate(x, tubeHeight, new SpriteTemplate(
-						Enemy.ENEMY_FLOWER, false));
-				ENEMIES++;
-			}
-
-			for (int y = 0; y < height; y++) {
-				if (y >= floor) {
-					setBlock(x, y, GROUND);
-
-				} else {
-					if ((x == xTube || x == xTube + 1) && y >= tubeHeight) {
-						int xPic = 10 + x - xTube;
-
-						if (y == tubeHeight) {
-							// tube top
-							setBlock(x, y, (byte) (xPic + 0 * 16));
-						} else {
-							// tube side
-							setBlock(x, y, (byte) (xPic + 1 * 16));
-						}
-					}
-				}
-			}
-		}
-
-		return length;
+	/**
+	 * Creates an enemy of the given type at the given point
+	 * 
+	 * @param at
+	 * @param type
+	 * @param winged
+	 */
+	private void enemy(Point at, int type, boolean winged) {
+		setSpriteTemplate(at.x, y(at.y), new SpriteTemplate(type, winged));
 	}
 
-	private int buildStraight(int xo, int maxLength, boolean safe) {
-		int length = random.nextInt(10) + 2;
-
-		if (safe)
-			length = 10 + random.nextInt(5);
-
-		if (length > maxLength)
-			length = maxLength;
-
-		int floor = height - 1 - random.nextInt(4);
-
-		// runs from the specified x position to the length of the segment
-		for (int x = xo; x < xo + length; x++) {
-			for (int y = 0; y < height; y++) {
-				if (y >= floor) {
-					setBlock(x, y, GROUND);
-				}
-			}
-		}
-
-		if (!safe) {
-			if (length > 5) {
-				decorate(xo, xo + length, floor);
-			}
-		}
-
-		return length;
+	/**
+	 * Converts the given height into a y coordinate
+	 * 
+	 * @param h
+	 * @return
+	 */
+	private int y(int h) {
+		return height - 1 - h;
 	}
-
-	private void decorate(int xStart, int xLength, int floor) {
-		// if its at the very top, just return
-		if (floor < 1)
-			return;
-
-		// boolean coins = random.nextInt(3) == 0;
-		boolean rocks = true;
-
-		// add an enemy line above the box
-		addEnemyLine(xStart + 1, xLength - 1, floor - 1);
-
-		int s = random.nextInt(4);
-		int e = random.nextInt(4);
-
-		if (floor - 2 > 0) {
-			if ((xLength - 1 - e) - (xStart + 1 + s) > 1) {
-				for (int x = xStart + 1 + s; x < xLength - 1 - e; x++) {
-					setBlock(x, floor - 2, COIN);
-					COINS++;
-				}
-			}
-		}
-
-		s = random.nextInt(4);
-		e = random.nextInt(4);
-
-		// this fills the set of blocks and the hidden objects inside them
-		if (floor - 4 > 0) {
-			if ((xLength - 1 - e) - (xStart + 1 + s) > 2) {
-				for (int x = xStart + 1 + s; x < xLength - 1 - e; x++) {
-					if (rocks) {
-						if (x != xStart + 1 && x != xLength - 2
-								&& random.nextInt(3) == 0) {
-							if (random.nextInt(4) == 0) {
-								setBlock(x, floor - 4, BLOCK_POWERUP);
-								BLOCKS_POWER++;
-							} else { // the fills a block with a hidden coin
-								setBlock(x, floor - 4, BLOCK_COIN);
-								BLOCKS_COINS++;
-							}
-						} else if (random.nextInt(4) == 0) {
-							if (random.nextInt(4) == 0) {
-								setBlock(x, floor - 4, (byte) (2 + 1 * 16));
-							} else {
-								setBlock(x, floor - 4, (byte) (1 + 1 * 16));
-							}
-						} else {
-							setBlock(x, floor - 4, BLOCK_EMPTY);
-							BLOCKS_EMPTY++;
-						}
-					}
-				}
-			}
-		}
-	}
-
+	
+	/*
+	 * Their code...
+	 */
+	
 	private void fixWalls() {
 		boolean[][] blockMap = new boolean[width + 1][height + 1];
 
@@ -501,28 +298,23 @@ public class MyLevel extends Level {
 		}
 	}
 
-	public RandomLevel clone() throws CloneNotSupportedException {
+	public Level clone() throws CloneNotSupportedException {
 
-		RandomLevel clone = new RandomLevel(width, height);
+		Level clone = new Level(width, height);
 
 		clone.xExit = xExit;
 		clone.yExit = yExit;
 		byte[][] map = getMap();
 		SpriteTemplate[][] st = getSpriteTemplate();
 
-		for (int i = 0; i < map.length; i++)
+		for (int i = 0; i < map.length; i++) {
 			for (int j = 0; j < map[i].length; j++) {
 				clone.setBlock(i, j, map[i][j]);
 				clone.setSpriteTemplate(i, j, st[i][j]);
 			}
-		clone.BLOCKS_COINS = BLOCKS_COINS;
-		clone.BLOCKS_EMPTY = BLOCKS_EMPTY;
-		clone.BLOCKS_POWER = BLOCKS_POWER;
-		clone.ENEMIES = ENEMIES;
-		clone.COINS = COINS;
+		}
 
 		return clone;
 
 	}
-
 }
